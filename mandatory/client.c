c5da6a9956202edae8b3d3a037f9ea58c7065d9f
keinn51 <soogood97@gmail.com>
2022-07-01 22:49:33 +0900

    ADD PRINT ERROR

--- mandatory/client.c
+++ mandatory/client.c
@@ -6,11 +6,22 @@
 /*   By: kyungsle <kyungsle@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 15:38:55 by kyungsle          #+#    #+#             */
-/*   Updated: 2022/07/01 21:58:23 by kyungsle         ###   ########.fr       */
+/*   Updated: 2022/07/01 22:43:29 by kyungsle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	exit_print_error(int i)
+{
+	if (i == 0)
+		ft_putstr_fd("Error sending message to server.\n", 1);
+	if (i == 1)
+		ft_putstr_fd("Error getting message from server.\n", 1);
+	if (i == 2)
+		ft_putstr_fd("You have to enter right arguments!\n", 1);
+	exit(1);
+}
 
 void	send_with_bit(int pid, char *argv)
 {
@@ -26,9 +37,11 @@ void	send_with_bit(int pid, char *argv)
 		while (shift < 8)
 		{
 			if ((argv[i] >> shift) & 1)
-				kill(pid, SIGUSR2);
+				if (kill(pid, SIGUSR2) == -1)
+					exit_print_error(0);
 			else
-				kill(pid, SIGUSR1);
+				if (kill(pid, SIGUSR1) == -1)
+					exit_print_error(0);
 			shift++;
 			usleep(300);
 		}
@@ -55,12 +68,13 @@ int	main(int argc, char **argv)
 
 	sa.sa_flags = SA_SIGINFO;
 	sa.sa_sigaction = client_handler;
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 ||
+	sigaction(SIGUSR2, &sa, NULL) == -1)
+		exit_print_error(1);
 	if (argc == 3)
 		send_with_bit(ft_atoi(argv[1]), argv[2]);
 	else
-		write(1, "You have to enter right arguments!\n", 2);
+		exit_print_error(2);
 	while (1)
 		pause();
 	return (0);
--- mandatory/server.c
+++ mandatory/server.c
@@ -6,11 +6,20 @@
 /*   By: kyungsle <kyungsle@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 15:38:57 by kyungsle          #+#    #+#             */
-/*   Updated: 2022/07/01 22:20:44 by kyungsle         ###   ########.fr       */
+/*   Updated: 2022/07/01 22:43:26 by kyungsle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	exit_print_error(int i)
+{
+	if (i == 0)
+		ft_putstr_fd("Error sending message to client.\n", 1);
+	if (i == 1)
+		ft_putstr_fd("Error getting message from client.\n", 1);
+	exit(1);
+}
 
 void	get_with_bit(int sig)
 {
@@ -23,7 +32,8 @@ void	get_with_bit(int sig)
 		write(1, &(g_signal.msg), 1);
 		g_signal.size = 0;
 		if (g_signal.msg == 0)
-			kill(g_signal.pid, SIGUSR2);
+			if (kill(g_signal.pid, SIGUSR2) == -1)
+				exit_print_error(0);
 	}
 }
 
@@ -43,8 +53,9 @@ int	main(void)
 	g_signal.msg = 0;
 	sa.sa_flags = SA_SIGINFO;
 	sa.sa_sigaction = server_handler;
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 ||
+		sigaction(SIGUSR2, &sa, NULL) == -1)
+		exit_print_error(1);
 	pid = getpid();
 	ft_putstr_fd("Process ID is ", 1);
 	ft_putnbr_fd(pid, 1);
